21092e69dfee0debfe9049ac86922387ddf23afb
José Ignacio <tgmelipilla@gmail.com>
2020-09-04 11:11:05 -0400

    First Commit

---
+++ app/src/main/java/com/example/yu_gi_ohultrabasichelper/viewoh/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yu_gi_ohultrabasichelper.viewoh
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.yu_gi_ohultrabasichelper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
